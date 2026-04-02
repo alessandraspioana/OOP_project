4f33ff104bbb87ae2a6d0c114fe3a4da3fa7ecad
Spataroaia Alessandra-Ioana <alessandraioanaspataroaia@gmail.com>
2026-04-02 16:20:07 +0300

    adaugare misiune

--- main.cpp
+++ main.cpp
@@ -1,47 +1,55 @@
 #include "HotelContinental.h"
 #include "Asasin.h"
 #include "Arma.h"
+#include "Misiune.h"
 #include <iostream>
 
 int main() {
-    std::cout << "--- Initializare Arsenal ---\n";
+    std::cout << "=== SIMULARE LUMEA ASASINILOR ===\n";
     Arma hk("HK P30", 15);
     Arma glock("Glock 17", 17);
-    Arma creion("Creion Ticonderoga", 1);
-
-    std::cout << "Arma principala are " << hk.getGloante() << " gloante.\n";
 
     Asasin john("John Wick", 100, hk);
     Asasin cassian("Cassian", 100, glock);
 
-    Asasin copieJohn = john;
+    HotelContinental hotel("Roma", 10000);
 
-    HotelContinental hotelNY("New York");
-    hotelNY.cazeaza(john);
-    hotelNY.cazeaza(cassian);
+    Misiune contract1("Eliminare Seful Camorra", 5, 2000);
+    contract1.adaugaCerinta("Fara urme");
+    contract1.adaugaCerinta("Doar cu arme albe");
 
-    std::cout << "\n--- Pregatiri ---\n";
+    Misiune contract2("Recuperare stick USB", 2, 500);
 
+    hotel.cazeaza(john);
+    hotel.cazeaza(cassian);
+    hotel.adaugaMisiune(contract1);
+    hotel.adaugaMisiune(contract2);
+
+    std::cout << "\n--- Status Initial ---\n";
+    hotel.afiseazaRegistru();
+    hotel.afiseazaAvizier();
+
+    std::cout << "\n--- Pregatire John Wick ---\n";
     john.antrenament();
+    hk.reincarca(10);
+    std::cout << "Verificare munitie: " << hk.getGloante() << " gloante.\n";
 
-    hk.reincarca(5);
+    std::cout << "\n--- Activitate Contractuala ---\n";
+    hotel.executaContract(0, 1);
+    hotel.executaContract(1, 0);
 
-    hotelNY.afiseazaRegistru();
+    Asasin clona = cassian;
+    hotel.organizeazaDuel(0, 1);
 
-    std::cout << "\n--- Incepe conflictul la Roma ---\n";
-    hotelNY.organizeazaDuel(0, 1);
-    hotelNY.organizeazaDuel(0, 1);
-    hotelNY.organizeazaDuel(0, 1);
+    std::cout << "\n--- Raport Final ---\n";
+    hotel.evacueazaDecedatii();
+    hotel.afiseazaRegistru();
+    std::cout << hotel << "\n";
 
-    std::cout << "\n--- Post-Conflict ---\n";
-    hotelNY.evacueazaDecedatii();
+    Asasin asasinRezerva("Generic", 10, Arma("Pistol Vechi", 5));
+    asasinRezerva = john;
+    std::cout << "Asasin rezerva dupa upgrade la John Wick: " << asasinRezerva << "\n";
 
-    hotelNY.afiseazaRegistru();
-    std::cout << "\nStare Hotel: " << hotelNY << "\n";
-
-    Asasin asasinNou("Nimeni", 50, creion);
-    asasinNou = john;
-    std::cout << "Dupa atribuire: " << asasinNou << "\n";
-
+    std::cout << "\n=== SIMULARE INCHEIATA ===\n";
     return 0;
 }
